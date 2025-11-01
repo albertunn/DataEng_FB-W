66d0692fc3237456b85bf2773be4d2e554c3526b
karenroht <karen.roht@gmail.com>
2025-11-01 19:49:07 +0200

    modified weather dim table columns

--- dbt/models/marts/dim_weather.sql
+++ dbt/models/marts/dim_weather.sql
@@ -1,2 +1,13 @@
-SELECT *
+SELECT 
+weather_key, 
+latitude,
+longitude,
+weather_date,
+weather_time,
+temperature,
+precipitation,
+rainfall,
+snowfall,
+wind_speed
+
 FROM {{ ref('stg_dim_weather') }}
